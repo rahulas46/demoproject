119dde9819e02433264a8e604318f2aea3274380
rahul <rahul@rahuls-MacBook-Pro.local>
2015-07-06 14:16:27 +0530

    applied relativelayout

--- newApp/newApp/FirstScreen.cs
+++ newApp/newApp/FirstScreen.cs
@@ -8,24 +8,37 @@ namespace newApp
 	{
 		public FirstScreen(){
 
-			Button b1 = new Button {Text = "Button1"};
+			Button b1 = new Button {Text = "Button1",
+				//HorizontalOptions = LayoutOptions.StartAndExpand
+			};
 			Button b2 = new Button {Text = "Button2"};
 			Button b3 = new Button{Text = "Button3"} ;
 			Button b4 = new Button {Text = "Button4"};
 
+			var relativeLayout = new RelativeLayout();
+			relativeLayout.Children.Add(b1,
+				xConstraint: Constraint.RelativeToParent(l => l.Bounds.Width/2),
+				yConstraint: Constraint.RelativeToParent(l=> l.Bounds.Height/2)
+			);
 
+			relativeLayout.Children.Add(b2,
+			xConstraint: Constraint.RelativeToView(b1,(l,v)=> v.X-10),
+		    yConstraint: Constraint.RelativeToView(b1,(l,v)=> v.Bounds.Bottom+20 ));
 
-			var sl = new StackLayout{
-				Children = {b1,b2,b3,b4}
-			};
+			relativeLayout.Children.Add(b3,
+			xConstraint: Constraint.RelativeToView(b2,(l,v)=> v.X-10),
+			yConstraint: Constraint.RelativeToView(b2,(l,v)=> v.Bounds.Bottom+20 ));
 
-			Content = sl;
+			relativeLayout.Children.Add(b4,
+			xConstraint: Constraint.RelativeToView(b3,(l,v)=> v.X-10),
+			yConstraint: Constraint.RelativeToView(b3,(l,v)=> v.Bounds.Bottom+20 ));
+			
 
 			Device.OnPlatform (iOS: () => {
-				sl.Padding = new Thickness(0,20,0,0);
+				relativeLayout.Padding = new Thickness(0,20,0,0);
 			});
 			
-			
+			Content = relativeLayout;
 
 		}
 	}
